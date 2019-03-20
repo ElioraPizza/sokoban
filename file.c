5d576c550d129922b6a0be3b6842979ab40b5512
King of Pizza <pavlon.vladenkof@gmail.com>
2019-03-20 18:08:54 +0300

    Сделана функция получения начальных размеров поля с файла для создания соотвествующих позже

--- file.c
+++ file.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <ncursesw/ncurses.h>
 #include <stdlib.h>
-	// считать с файла поле
-	void funk (int maxcol,int maxrow)
+// считать с файла поле
+void getmysize (int* row,int* col)
 {
-	FILE *myfile;
-		
+								FILE *myfile;
+								myfile=fopen("./map.txt", "r+");
+								char test[40];
+								fscanf(myfile, "%s", test);
+								while(test[*col]!=0)
+																(*col)++;
+								int i;
+								for (i=1; fscanf(myfile, "%s", test)!=EOF; i++);
+								*row=i;
+								fclose(myfile);
 }
-	int main () 
+int main ()
 {
-	// кейсы с клавишами + передвижение + сдвиг ящиков
+								int sizerow=0,sizecol=0;
+								getmysize(&sizerow,&sizecol);
+								printf("%d\n%d\n",sizecol,sizerow);
+								// кейсы с клавишами + передвижение + сдвиг ящиков
 
-	// game over
+								// game over
 
-	// переписать поле заново
-	return 0;
+								// переписать поле заново
+								return 0;
 }
