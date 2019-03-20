264c8725f053a34d30671aa3bd1e308943a9568c
student <you@example.com>
2019-03-20 11:27:30 +0300

    Скелет программы.

---
+++ file.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ncursesw/ncurses.h>
+#include <stdlib.h>
+	// считать с файла поле
+	void funk (int maxcol,int maxrow)
+{
+	FILE *myfile;
+		
+}
+	int main () 
+{
+	// кейсы с клавишами + передвижение + сдвиг ящиков
+
+	// game over
+
+	// переписать поле заново
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,3 @@
+file: file.c
+	gcc -o fileex.out file.c -lncursesw -std=c99
+	./fileex.out
